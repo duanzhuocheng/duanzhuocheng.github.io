dbf3d5be90fe8c09f6bfee3f4b4a5cb4e03ca4d6
duanzhuocheng <894800644@qq.com>
2023-01-24 21:35:18 +0800

    updating site on Tue Jan 24 21:35:18     2023

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+git config --global --unset http.proxy
+git config --global --unset https.proxy
+git config --global http.proxy
+git config --global https.proxy
+
 git add .
 msg="updating site on $(date)" 
 git commit -m "$msg"
